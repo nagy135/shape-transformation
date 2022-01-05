fd94e4e76c54705a9ae3ae7af17f9a830f418a93
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-01-05 18:45:20 +0100

    feat: css changes

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -1,11 +1,6 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import Webcam from "react-webcam";
-
-const videoConstraints = {
-  width: 1280,
-  height: 720,
-  facingMode: "environment"
-};
+import './css/Webcam.css';
 
 const WebcamCapture = () => {
   const webcamRef = useRef<Webcam>(null);
@@ -37,29 +32,30 @@ const WebcamCapture = () => {
 
   return (
     <>
-      <Webcam
-        audio={false}
-        height={720}
-        ref={webcamRef}
-        screenshotFormat="image/png"
-        width={1280}
-        videoConstraints={videoConstraints}
-      />
-      <button style={{ position: 'absolute', top: 0, right: 0, zIndex: 9999 }} onClick={capture}>Capture photo</button>
+      <div id="total-wrapper">
+        <Webcam
+          audio={false}
+          height={720}
+          ref={webcamRef}
+          screenshotFormat="image/png"
+          width={1280}
+          videoConstraints={{
+            width: 1280,
+            height: 720,
+            facingMode: "environment"
+          }}
+        />
+        <button id="capture-btn" onClick={capture}>Capture photo</button>
 
-      {ghost &&
-        <img style={{
-          position: 'absolute',
-          left: 0,
-          top: 0,
-          opacity: 0.5
-        }} key="ghost" id="ghost" alt="result" src={ghost} />
-      }
-
-      <div id="slideshow">
-        {images.length &&
-          <img style={{ position: 'absolute', top: 720, left: 0 }} id="result" alt="result" src={images[slide]} />
+        {ghost &&
+          <img key="ghost" className="ghost" alt="ghost" src={ghost} />
         }
+
+        <div id="slideshow">
+          {images.length &&
+            <img id="result" alt="result" src={images[slide]} />
+          }
+        </div>
       </div>
     </>
   );
