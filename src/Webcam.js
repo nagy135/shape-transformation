a5ad02f3919a24a4d8947c7eedcfaa40594763a9
Viktor Nagy <viktor.nagy@01people.com>
2022-01-05 12:06:44 +0100

    feat: ghost image

--- src/Webcam.js
+++ src/Webcam.js
@@ -11,22 +11,22 @@ const WebcamCapture = () => {
   const webcamRef = useRef(null);
 
   const [images, setImages] = useState([]);
+  const [ghost, setGhost] = useState(null);
 
   const capture = useCallback(
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
       setImages(old => [...old, imageSrc]);
+      setGhost(imageSrc);
     },
     [webcamRef]
   );
 
   const [slide, setSlide] = useState(0);
-  let [intervalId, setIntervalId] = useState(null);
 
   useEffect(() => {
-    if (intervalId) clearInterval(intervalId);
 
-    setIntervalId(setInterval(() => {
+    const intId = setInterval(() => {
       setSlide(old => {
         const total = isNaN(images.length) || images.length === 0 ? 1 : images.length;
         const newId = (old + 1) % total;
@@ -38,16 +38,14 @@ const WebcamCapture = () => {
           let img = imgs[i];
           img.style.display = 'none';
         }
-        const selectedOne = document.querySelector('div#slideshow > img[i="'+ newId + '"]');
+        const selectedOne = document.querySelector('div#slideshow > img[i="' + newId + '"]');
         if (selectedOne) selectedOne.style.display = 'block';
 
         return newId;
       });
-    }, 300));
+    }, 300);
+    return () => { if (intId) clearInterval(intId) };
   }, [images]);
-
-
-  useEffect(() => console.log(images), [images]);
 
   return (
     <>
@@ -57,12 +55,26 @@ const WebcamCapture = () => {
         ref={webcamRef}
         screenshotFormat="image/png"
         width={1280}
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+        }}
         videoConstraints={videoConstraints}
       />
-      <button onClick={capture}>Capture photo</button>
+      <button style={{ position: 'absolute', top: 0, right: 0, zIndex: 9999 }} onClick={capture}>Capture photo</button>
+
+      {ghost &&
+        <img style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          opacity: 0.5
+        }} key="ghost" id="ghost" alt="result" src={ghost} />
+      }
 
       <div id="slideshow">
-        {images.map((imageSrc, i) => <img style={{ display: i == 0 ? 'default' : 'none' }} i={i} key={i} id="result" alt="result" src={imageSrc} />)}
+        {images.map((imageSrc, i) => <img style={{ position: 'absolute', top: 800, display: i == 0 ? 'default' : 'none' }} i={i} key={i} id="result" alt="result" src={imageSrc} />)}
       </div>
     </>
   );
